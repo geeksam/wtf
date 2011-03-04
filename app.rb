e081f7e76369bebc4e988de0803878841ad59ef8
Sam Livingston-Gray <sam@renewfund.com>
2011-03-03 22:15:05 -0800

    Move some files around.

--- app.rb
+++ app.rb
@@ -1,15 +1,7 @@
 require 'sinatra'
 require './config/environment'
+require './helpers'
 
-helpers do
-
-  def link_to(url, text=url, opts={})
-    attributes = ""
-    opts.each { |key, value| attributes << key.to_s << "=\"" << value << "\" " }
-    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
-  end
-
-end
 
 get '/' do
   erb :index
--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection dbconfig[ENV['RACK_ENV'] || 'development']
 
 AppRoot = File.expand_path(File.join(File.dirname(__FILE__), *%w[..]))
-$: << File.join(AppRoot, *%w[db models])
+$: << File.join(AppRoot, 'models')
 
 require 'project'
